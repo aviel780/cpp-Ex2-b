9f09498cb7dc8c90a9edeb4b835fd7818f275e72
aviel <aviel780@gmail.com>
2023-04-11 12:33:45 +0300

    card works

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,1 +1,53 @@
+#ifndef Card_hpp
+#define Card_cpp
+#include <iostream>
 
+#include "string"
+using namespace std;
+namespace ariel
+{
+class card{
+    int value;
+    std::string sigen;
+
+    public:
+
+    card(int x , string y){
+        value = x;
+        sigen = y;  
+    }
+
+    ~card();
+
+    int getvalue(){
+        return value;
+    }
+
+    string getsigen(){
+        return sigen;
+    }
+    string card_tostring(){
+        if(this->value == 1){
+            return "played Ace of" + this->sigen;
+        }
+        if(this->value == 11){
+            return "played Jack of" + this->sigen;
+        }
+        if(this->value == 12){
+            return "played Queen of" + this->sigen;
+        }
+        if(this->value == 13){
+            return "played King of" + this->sigen;
+        }
+        return "played" + this->value + "of" + this->sigen;
+
+
+
+    }
+};
+
+
+
+ }
+
+#endif
--- sources/card.hpp
+++ sources/card.hpp
@@ -1,7 +1,22 @@
 #ifndef Card_hpp
 #define Card_hpp
-
+#include "string"
+using namespace std;
+namespace ariel
+{
 class card{
+    int value;
+    std::string sigen;
 
+    public:
+
+    card(int x , string y);
+
+    ~card();
+
+    int getvalue();
+    string getsigen();
+     string card_tostring();
 };
+}
 #endif //Card_hpp
